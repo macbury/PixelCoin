e06dd6b9a3e548c9cdb2fd07c66edefc3ce3e77a
Arkadiusz Buras <macbury@gmail.com>
2018-06-23 17:43:31 +0200

    Proper network

--- truffle.js
+++ truffle.js
@@ -16,7 +16,7 @@ module.exports = {
       network_id: "4", // Rinkeby ID 4
       from: "0x99a4572656eb49FFEEFbe9588f8e7ab0F8D6Eb5e", // account from which to deploy
       gas: 6712390
-    }
+    },
 
     test: {
       host: '127.0.0.1',
